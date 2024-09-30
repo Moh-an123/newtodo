e66deeaa0c184b0e03920afa85dbe2b7df00c69d
Moh-an123 <113376269+Moh-an123@users.noreply.github.com>
2024-09-30 17:27:18 +0530

    todolist

---
+++ Frontend/src/Home.jsx
@@ -0,0 +1,88 @@
+import React, { useEffect, useState, useCallback } from "react";
+import Todo from "./Todo";
+import axios from "axios";
+import { FaRegCircle, FaRegCheckCircle } from "react-icons/fa";
+import { MdDeleteOutline } from "react-icons/md";
+import { FaGithub } from "react-icons/fa";
+const Home = () => {
+  const [chck, setChck] = useState(false);
+  const [todos, setTodos] = useState([]);
+
+  const fetchTodos = useCallback(() => {
+    axios
+      .get("https://todolist-tew6.onrender.com/get")
+      .then((result) => {
+        setTodos(result.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetchTodos();
+  }, [fetchTodos]);
+
+  const handlecheck = useCallback((id) => {
+    setChck((prevChck) => !prevChck);
+    axios
+      .put(`https://todolist-tew6.onrender.com/update/${id}`)
+      .then((result) => {
+        console.log(result);
+        fetchTodos(); // Refresh the todo list after update
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [fetchTodos]);
+
+  const handledelete = useCallback((id) => {
+    setChck((prevChck) => !prevChck);
+    console.log("delete");
+    axios
+      .delete(`https://todolist-tew6.onrender.com/delete/${id}`)
+      .then((result) => {
+        console.log(result);
+        fetchTodos(); // Refresh the todo list after delete
+      })
+      .catch((err) => console.log(err));
+  }, [fetchTodos]);
+
+  return (
+    <div className="m1">
+    <a href="https://github.com/Moh-an123">
+    <FaGithub  className="github"/></a>
+  
+    <div className="main">
+      <h1 >Todo List</h1>
+      <Todo />
+      <div className="lb">
+      {todos.length > 0 ? (
+        todos.map((item) => {
+          return (
+            <div className="list" onClick={() => handlecheck(item._id)}>
+              {item.finish ? (
+                <FaRegCheckCircle className="circle" />
+              ) : (
+                <FaRegCircle className="circle" />
+              )}
+              <h2 id={item} className={item.finish?"todo underline":"todo"}>{item.task}</h2>
+              <MdDeleteOutline
+                className="delete"
+                onClick={() => handledelete(item._id)}
+              />
+            </div>
+          );
+        })
+      ) : (
+        <h2>No Items</h2>
+      )
+     
+      }
+       </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
